b9e01a244ee3d2e6310c0430f48fdaa9b8b13ba6
Abisake Villalan <abisake123@gmail.com>
2026-03-25 07:43:19 +0530

    Working with enums and maps

--- miniProject/python/main.py
+++ miniProject/python/main.py
@@ -2,6 +2,8 @@ import sys
 
 import proto.account_pb2 as acc
 import proto.user_pb2 as user
+import proto.product_pb2 as product
+import proto.phone_book_pb2 as phone_book
 
 def account():
     return acc.Account(
@@ -29,6 +31,32 @@ def user2():
     usr.follows.add(id=68, name="Sanji")
     return usr
     
+def product1():
+    return product.Product(
+        id=11,
+        product_type=product.Product.ProductType.SHIRTS
+    )
+
+def product2():
+    return product.Product(
+        id=12,
+        product_type=3
+    )
+
+def phone_book1():
+    return phone_book.PhoneBook(
+        contact_info={
+            'Uchicha Itachi'    :   '1234567890',
+            'Hatake Kakashi'    :   '3242543632',
+        }
+    )
+
+def phone_book2():
+    ph_book = phone_book.PhoneBook()
+    ph_book.contact_info['Zolydck Killua'] ='8923659243'
+    ph_book.contact_info['Ging Freeces'] = '3829572034'
+    
+    return ph_book
 
 if __name__ == "__main__":
     # A map where key=arguments, value=function needs to be called
@@ -36,6 +64,10 @@ if __name__ == "__main__":
         'account'   :   account,
         'user'      :   user1,
         'user2'     :   user2,
+        'product1'  :   product1,
+        'product2'  :   product2,
+        'phone1'    :   phone_book1,
+        'phone2'    :   phone_book2
     }
     
     # on calling this program, more than 2 flags should throw error
